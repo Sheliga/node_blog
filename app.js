8667c1fb8d7a11acb7fe69f2f5dde3f992dc43a4
Paulo Cezar Sheliga <paulosheliga@gmail.com>
2022-02-07 23:15:14 -0300

    feat: estrutura inicial do projeto

---
+++ app.js
@@ -0,0 +1,23 @@
+//carregando modulos
+    const express = require("express")
+    const handlebars = require("express-handlebars")
+    const bodyParser = require("body-parser")
+    const app = express()
+    //const mongoose = require("mongoose")
+
+//Configurações
+    //Body Parser
+        app.use(bodyParser.urlencoded({extended:true})) 
+        app.use(bodyParser.json()) 
+    //Handlebars
+        app.engine('handlebars', handlebars.engine({defaultLayout:'main'})) 
+        app.set('view engine', 'handlebars')
+    //Mongoose
+
+//Rotas
+
+//Outros
+const PORT = 8081
+app.listen(PORT, () => {
+    console.log("servidor rodando")
+})
