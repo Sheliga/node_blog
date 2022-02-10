d29bf4b2c8ad18d274b03d1fd82dd0a9422d15cc
Paulo Cezar Sheliga <paulosheliga@gmail.com>
2022-02-10 16:16:44 -0300

    feat: configuração das sessões

--- app.js
+++ app.js
@@ -6,8 +6,24 @@
     const admin = require("./routes/admin")
     const path = require('path')
     const mongoose = require("mongoose")
+    const session = require("express-session")
+    const flash = require("connect-flash")
+
 
 //Configurações
+    //Sessão
+        app.use(session({
+            secret: "node",
+            resave:true,
+            saveUninitialized:true
+        }))
+        app.use(flash())
+    //middleware
+        app.use((req, res, next)=>{
+            res.locals.success_msg = req.flash("success_msg") //variavel global
+            res.locals.error_msg = req.flash("error_msg") //variavel global
+            next()
+        })
     //Body Parser
         app.use(bodyParser.urlencoded({extended:true})) 
         app.use(bodyParser.json()) 
