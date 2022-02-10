cfac5f16a59a74f8ed4c75b6bbc2284d0ed05332
Paulo Cezar Sheliga <paulosheliga@gmail.com>
2022-02-10 12:42:32 -0300

    feat: rota post de salvar categorias no banco

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,36 @@
 const express = require("express")
 const router = express.Router()
+const mongoose = require("mongoose")
+require("../models/Categoria")
+const Categoria = mongoose.model("categorias")
 
-router.get('/', (req, res)=>{
+router.get("/", (req, res)=>{
     res.render("admin/index")
 })
 
-router.get('/posts', (req, res)=>{
+router.get("/posts", (req, res)=>{
     res.send("Página de posts")
 })
 
-router.get('/categorias', (req, res)=>{
+router.get("/categorias", (req, res)=>{
     res.render("admin/categorias")
 })
 
-router.get('/categorias/add', (req, res)=>{
+router.get("/categorias/add", (req, res)=>{
     res.render("admin/addcategorias")
 })
+
+router.post("/categorias/nova", (req, res)=>{
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug,
+
+    }
+    new Categoria(novaCategoria).save().then(()=>{
+        console.log("categoria salva com sucesso")
+    }).catch((err)=>{
+        console.log("erro ao salvar categoria: "+err)
+    })
+})
+
 module.exports = router
