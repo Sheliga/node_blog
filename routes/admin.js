d5d3da181733a69238e9bcbac5c424cdb2e43fc3
Paulo Cezar Sheliga <paulosheliga@gmail.com>
2022-02-27 18:29:19 -0300

    feat: formulario de edição de postagem

--- routes/admin.js
+++ routes/admin.js
@@ -161,4 +161,20 @@ router.post("/postagens/nova/", (req, res) =>  {
     }
 
 })
+
+router.get("/postagens/edit/:id", (req, res) =>  {
+    Postagem.findOne({_id:req.params.id}).lean().then((postagem) => {
+        Categoria.find().lean().then((categorias) => {
+            res.render("admin/editpostagens", {categorias:categorias, postagem: postagem})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao listar as categorias")
+            res.redirect("/admin/postagens")
+        })
+        
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulario de edição")
+        res.redirect("/admin/postagens")
+    })
+    
+})
 module.exports = router
