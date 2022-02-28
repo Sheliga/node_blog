5d1b5640d456604c3219120fd16472fc8bdbb2b3
Paulo Cezar Sheliga <paulosheliga@gmail.com>
2022-02-28 11:02:19 -0300

    feat: criação de homepage

--- app.js
+++ app.js
@@ -8,6 +8,10 @@
     const mongoose = require("mongoose")
     const session = require("express-session")
     const flash = require("connect-flash")
+    
+    require("./models/Postagens")
+    const Postagem = mongoose.model("postagens")
+
 
 
 //Configurações
@@ -39,6 +43,21 @@
     //Public
         app.use(express.static(path.join(__dirname,"/public")))
 //Rotas
+    app.get('/', (req, res)=> {
+        Postagem.find().populate("categoria").sort({data:"desc"}).lean().then((postagens)=>{
+            res.render("index", {postagens:postagens})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/404")
+        })
+      
+    })
+    app.get('/404', (req, res) => {
+        res.send('Erro 404!')
+    })
+    app.get('/posts', (req, res) => {
+        res.send("Lista Posts")
+    })
     app.use('/admin', admin)
 
 //Outros
